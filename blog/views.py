4757e83d663ab0cfaa1a339a58e6c1b71deddc38
rambody542 <rambody542@gmail.com>
2023-06-12 14:49:28 +0900

    thrid commit

--- blog/views.py
+++ blog/views.py
@@ -4,12 +4,20 @@ from django.http import HttpResponse
 #chetterbot models in here#
 
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
 
 
 # Chetterbot Objects in here.#
 
-bot = ChatBot('chatbot', read_only=False,logic_adapters=['chatterbot.logic.BestMatch'])
+bot = ChatBot('chatbot', read_only=False,
+              logic_adapters=[
+                {
+                  'import_path':'chatterbot.logic.BestMatch',
+                  #'default_response': 'Sorry, I dont know what you means',
+                  #'maximum_similarity_threshold':0.90
+                  
+                }
+                ])
 
 # Chetterbot & Training & Teaching. #
 list_to_train = [
@@ -19,13 +27,28 @@ list_to_train = [
     "what's your name",
     "I'm just a chatbot",
     "What is your favorite food?",
-    "I like cheese"
+    "I like cheese",
+    "what's your favorite sports?",
+    "swimming",
+    "Do you have Children?",
+    "No",
+    "Where Do you Live?",
+    "Busan",
+    "Where Do you live?",
+    "Seoul",
+    "What is your job?",
+    "Maketor"
     
 ]
 
-list_trainer = ListTrainer(bot)
-list_trainer.train(list_to_train)
 
+chatterbotCorpusTrainer = ChatterBotCorpusTrainer(bot)
+
+
+#list_trainer = ListTrainer(bot)
+#list_trainer.train(list_to_train)
+
+chatterbotCorpusTrainer.train('chatterbot.corpus.english')
 
 
 # Create your views here.
@@ -33,9 +56,12 @@ list_trainer.train(list_to_train)
 def index(request):
   return render(request, 'blog/index.html')
 
+
 def specific(request):
-  return HttpResponse(list1)
+  return HttpResponse("list1")
+
 
 def getResponse(request):
   userMessage = request.GET.get('userMessage')
-  return HttpResponse(userMessage)
+  chatResponse = str(bot.get_response(userMessage))
+  return HttpResponse(chatResponse)
